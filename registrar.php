713e938535e9c84388e99db0dac991c46a913ef5
Isaacllolo <isaaacllolo@gmail.com>
2022-11-02 09:58:06 -0500

    Mas backend

--- registrar.php
+++ registrar.php
@@ -1,27 +1,31 @@
 <?php
 
-$usuario = isset($_POST['txt_usuario']) ? $_POST['txt_usuario'] : '';
-$nombre = isset($_POST['txt_nombre']) ? $_POST['txt_nombre'] : '';
-$edad = isset($_POST['int_edad']) ? $_POST['int_edad'] : '';
+$usuario = isset($_POST['usuario']) ? $_POST['usuario'] : '';
+$password = isset($_POST['password']) ? $_POST['password'] : '';
+
+
+$host="localhost";
+$port=3306;
+$socket="";
+$user="root";
+$password="";
+$dbname="sakila";
 
 try {
-
-    $conexion = new PDO("mysql:host=localhost;port=3306;dbname=prueba_db", "root", "");
-    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-
-    $pdo = $conexion->prepare('INSERT INTO usuarios(usuario, nombre, edad) VALUES(?, ?, ?)');
-    $pdo->bindParam(1, $usuario);
-    $pdo->bindParam(2, $nombre);
-    $pdo->bindParam(3, $edad);
-    $pdo->execute() or die(print($pdo->errorInfo()));
+    $dbh = new PDO("mysql:host={$host};port={$port};dbname={$dbname}", $user, $password));
+    $con = $conexion->prepare('INSERT INTO usuarios(usuario,password) VALUES(?, ?, ?)');
+    $con->bindParam(1, $usuario);
+    $con->bindParam(2, $password);
+    $con->execute() or die(print($pdo->errorInfo()));
 
     echo json_encode('true');
 
-} catch(PDOException $error) {
-    echo $error->getMessage();
-    die();
-} 
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
+    
+
+
   
 
 ?>
